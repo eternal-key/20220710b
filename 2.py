f1023f9a7b487682b79eacfeb51a413eaad71331
eternal-key <lxu0730@gmail.com>
2022-07-10 17:09:55 +0800

    尝试

---
+++ 2.py
@@ -0,0 +1,26 @@
+'''
+Description: 
+Author: luoxu
+Date: 2022-07-08 17:08:37
+LastEditTime: 2022-07-08 22:51:56
+LastEditors: luoxu
+Reference:                          
+'''
+b = 3
+a = 4
+a = 4
+b = 3
+b = 3
+b = 3
+b = 3
+b = 3
+c = 5
+c = 5
+c = 5
+
+a=8
+
+def a():
+    lit = [1,23,4]
+
+
